4d12c7d5b6aab964374fc140408ebb9780c7a1c8
Zalonics <anthony.dev.9898@gmail.com>
2022-07-20 18:44:26 -0700

    Stable Ver.

--- src/config.js
+++ src/config.js
@@ -3,22 +3,22 @@ const fs = require('fs')
 const input = fs.readFileSync('../input.html', 'utf-8')
 // Don't include the / at the end on domain
 // Leave blank if you want to download everything (TODO)
-const domain = ''
-const cyberschool_domain = ''
+const domain = 'http://fcrsd.org/'
+const cyberschool_domain = 'https://forestcity.cyberschool.com/'
 
 const newRelativePath = `/files/public_files/`
 const downloadsDirectory = '../Downloads'
 
 // If you want to auto create pages or auto download content
-const username = ''
-const password = ''
+const username = 'arodriguez'
+const password = 'awesome9'
 
 module.exports = {
-	input,
-	domain,
-	cyberschool_domain,
-	newRelativePath,
-	downloadsDirectory,
-	username,
-	password,
+    input,
+    domain,
+    cyberschool_domain,
+    newRelativePath,
+    downloadsDirectory,
+    username,
+    password,
 }
